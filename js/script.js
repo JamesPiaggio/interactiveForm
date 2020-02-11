223b1c379e35191349ec5f184f003d04689448db
James P <james.piaggio13@gmail.com>
2020-02-10 20:31:32 -0500

    Focus nameInput by Default

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Global Variables
+const nameInput = document.getElementById('name');
+const otherRole = document.getElementById('other-title');
+// Focuses Name field by default
+nameInput.focus();
+// Hides other job role text field
+otherRole.style.display = 'none';
