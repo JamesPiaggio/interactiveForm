52deff695c741348d80f10169d15ed315241a609
James P <james.piaggio13@gmail.com>
2020-02-12 10:17:53 -0500

    Hid colorMenu options

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 //Global Variables
 const nameInput = document.getElementById('name');
 const otherRole = document.getElementById('other-title');
+const designMenu = document.getElementById('design');
+const colorMenu = document.getElementById('color');
 // Focuses Name field by default
 nameInput.focus();
 // Hides other job role text field
 otherRole.style.display = 'none';
+designMenu.firstElementChild.style.display = 'none';
+// Adds new option to ColorMenu
+const selectTheme = document.createElement('option');
+selectTheme.appendChild(document.createTextNode('Please select a theme'));
+colorMenu.insertBefore(selectTheme, colorMenu.firstChild);
+for (let i = 1; i < colorMenu.length; i++) {
+    colorMenu[i].hidden = true;
+}
