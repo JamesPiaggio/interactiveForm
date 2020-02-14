dd81f886861acb1dd0c014e0d7d61a72008ab78a
James P <james.piaggio13@gmail.com>
2020-02-13 21:04:24 -0500

    Added js for t-shirt info section

--- js/script.js
+++ js/script.js
@@ -3,15 +3,60 @@ const nameInput = document.getElementById('name');
 const otherRole = document.getElementById('other-title');
 const designMenu = document.getElementById('design');
 const colorMenu = document.getElementById('color');
+
+
 // Focuses Name field by default
 nameInput.focus();
+
 // Hides other job role text field
-otherRole.style.display = 'none';
-designMenu.firstElementChild.style.display = 'none';
+otherRole.hidden = true;
+
+// Shows Select theme <option>
+designMenu.firstElementChild.selected = true;
+
 // Adds new option to ColorMenu
-const selectTheme = document.createElement('option');
-selectTheme.appendChild(document.createTextNode('Please select a theme'));
-colorMenu.insertBefore(selectTheme, colorMenu.firstChild);
-for (let i = 1; i < colorMenu.length; i++) {
-    colorMenu[i].hidden = true;
+const themeOption = document.createElement('option');
+
+// Adds text to the created <option> element
+themeOption.appendChild(document.createTextNode('Please select a T-shirt theme'));
+
+// Inserts new <option> before the first child inside the select menu
+colorMenu.insertBefore(themeOption, colorMenu.firstChild);
+themeOption.selected = true;
+
+// Hides colorMenu colors
+const clearColors = () => {
+    for (let i = 1; i < colorMenu.length; i++) {
+        colorMenu[i].hidden = true;
+    }
 }
+
+clearColors();
+
+designMenu.addEventListener('change', (e) => {
+    // If statement to only execute once
+    if (themeOption.hidden == false) {
+        // Hides Select theme <option>
+        designMenu.firstElementChild.hidden = true;
+        themeOption.hidden = true;
+        // Creates new <option> with new message
+        const colorOption = document.createElement('option');
+        colorOption.appendChild(document.createTextNode('Please select a color'));
+        colorMenu.insertBefore(colorOption, colorMenu.firstChild);
+        // Shows new message but hides it when selecting a color
+        colorOption.selected = true;
+        colorOption.hidden = true;
+    }
+    // If statement for Theme/Color
+    if (e.target.value == 'js puns') {
+        clearColors();
+        for (let i = 2; i < 5; i++) {
+            colorMenu[i].hidden = false;
+        }
+    } else if (e.target.value == 'heart js') {
+        clearColors();
+        for (let i = 5; i < 8; i++) {
+            colorMenu[i].hidden = false;
+        }
+    }
+});
