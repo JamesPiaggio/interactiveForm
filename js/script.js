a151eb041da4559fe2d6d2a9ba6cb76744d1d1cb
James P <james.piaggio13@gmail.com>
2020-02-13 21:10:57 -0500

    updated eventListener

--- js/script.js
+++ js/script.js
@@ -16,10 +16,8 @@ designMenu.firstElementChild.selected = true;
 
 // Adds new option to ColorMenu
 const themeOption = document.createElement('option');
-
 // Adds text to the created <option> element
 themeOption.appendChild(document.createTextNode('Please select a T-shirt theme'));
-
 // Inserts new <option> before the first child inside the select menu
 colorMenu.insertBefore(themeOption, colorMenu.firstChild);
 themeOption.selected = true;
@@ -39,23 +37,18 @@ designMenu.addEventListener('change', (e) => {
         // Hides Select theme <option>
         designMenu.firstElementChild.hidden = true;
         themeOption.hidden = true;
-        // Creates new <option> with new message
-        const colorOption = document.createElement('option');
-        colorOption.appendChild(document.createTextNode('Please select a color'));
-        colorMenu.insertBefore(colorOption, colorMenu.firstChild);
-        // Shows new message but hides it when selecting a color
-        colorOption.selected = true;
-        colorOption.hidden = true;
     }
     // If statement for Theme/Color
     if (e.target.value == 'js puns') {
         clearColors();
-        for (let i = 2; i < 5; i++) {
+        colorMenu[1].selected = true;
+        for (let i = 1; i < 4; i++) {
             colorMenu[i].hidden = false;
         }
     } else if (e.target.value == 'heart js') {
         clearColors();
-        for (let i = 5; i < 8; i++) {
+        colorMenu[4].selected = true;
+        for (let i = 4; i < 7; i++) {
             colorMenu[i].hidden = false;
         }
     }
